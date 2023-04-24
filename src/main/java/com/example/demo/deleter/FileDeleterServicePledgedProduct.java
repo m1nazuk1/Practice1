4b4aa6917e5630e63da470a9b327c60b5194e927
m1nazuk1 <alekperov-nizam@mail.ru>
2023-04-25 00:50:07 +0300

    RomanAndNizami

--- src/main/java/com/example/demo/deleter/FileDeleterServicePledgedProduct.java
+++ src/main/java/com/example/demo/deleter/FileDeleterServicePledgedProduct.java
@@ -3,7 +3,7 @@ package com.example.demo.deleter;
 import java.io.File;
 
 public class FileDeleterServicePledgedProduct {
-    private final String filePath = "src/main/resources/entities/users/";
+    private final String filePath = "src/main/resources/entities/pledgedProduct/";
 
     public void delete(Long id){
         File folder = new File(filePath + id + ".txt");
